04ce4f92e321c538ad77e407df1c978fb51fd7c6
juanpuerto23 <juan2224511@correo.uis.edu.co>
2022-04-26 11:29:10 -0500

    Trabajo suma enteros

---
+++ README.md
@@ -0,0 +1,7 @@
+# Programa No. 1
+
+Crear un programa para sumar dos números enteros 
+
+# Diagrama
+
+![Diagrama](suma_enteros.png)
---
+++ suma_enteros.py
@@ -0,0 +1,20 @@
+""""Programa para calcular la suma de dos numeros enteros"""
+
+print("----------------------------------------------")
+print("-----------  Suma de dos enteros -------------")
+print("----------------------------------------------")
+
+# Input
+
+X = input("Ingrese el primer numero: ")
+X = int(X)
+Y = input("Ingrese el segundo numero: ")
+Y = int(Y)
+
+# Processing
+
+Z = (X + Y)
+
+# Output
+
+print("La suma de " + str(X) + " y " + str(Y) + " equivale a " + str(Z))
